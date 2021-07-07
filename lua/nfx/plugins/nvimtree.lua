2394e2bbe7b5e6968a0f7e13a8288b3ba270408c
Nelson Fernandez <nelson@netflux.com.ar>
2021-07-06 22:07:34 -0300

    config nvim-tree

--- lua/nfx/plugins/nvimtree.lua
+++ lua/nfx/plugins/nvimtree.lua
@@ -2,25 +2,28 @@ vim.g.nvim_tree_width = 40
 vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' } -- empty by defaul
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
-vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_follow = 0
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_group_empty = 1
+vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_icons = {
-  default        = '',
-  symlink        = '◎',
-  git            = {
-    unstaged     = "✗",
-    staged       = "✓",
-    unmerged     = "ϟ",
-    renamed      = "➜",
-    untracked    = "★"
-  },
-  folder         = {
-    default      = "▶",
-    open         = "▼",
-    empty        = "▷",
-    empty_open   = "▽",
-    symlink      = "▣",
-    symlink_open = "▢",
-  }
-}
+-- vim.g.nvim_tree_icons = {
+--   default        = '',
+--   symlink        = '◎',
+--   git            = {
+--     unstaged     = "✗",
+--     staged       = "✓",
+--     unmerged     = "ϟ",
+--     renamed      = "➜",
+--     untracked    = "★"
+--   },
+--   folder         = {
+--     default      = "▶",
+--     open         = "▼",
+--     empty        = "▷",
+--     empty_open   = "▽",
+--     symlink      = "▣",
+--     symlink_open = "▢",
+--   }
+-- }
