33f6a88d69fbdbc857afd605b79462dfb76961b2
Nelson Fernandez <nelson@netflux.com.ar>
2021-09-30 21:03:08 -0300

    updated nvim-tree

--- lua/nfx/plugins/nvimtree.lua
+++ lua/nfx/plugins/nvimtree.lua
@@ -1,13 +1,11 @@
-vim.g.nvim_tree_width = 40
 vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' } -- empty by defaul
-vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_quit_on_open = 1
-vim.g.nvim_tree_follow = 0
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
+
 -- vim.g.nvim_tree_icons = {
 --   default        = '',
 --   symlink        = '◎',
@@ -27,3 +25,16 @@ vim.g.nvim_tree_git_hl = 1
 --     symlink_open = "▢",
 --   }
 -- }
+
+require'nvim-tree'.setup {
+  lsp_diagnostics     = true,
+  view = {
+    width = 40,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  }
+}
