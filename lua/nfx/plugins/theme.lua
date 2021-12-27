9e27f113f1bd0d283d5267f641bed1f5a7e59854
Nelson Fernandez <nelson@netflux.com.ar>
2021-12-27 20:17:36 -0300

    theme cleanup

--- lua/nfx/plugins/theme.lua
+++ lua/nfx/plugins/theme.lua
@@ -1,21 +1,27 @@
 vim.o.background = "dark"
 
+local clrFloat = "#15161E"
+
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_sidebars = {
   "qf",
   "terminal",
   "packer",
   "help",
-  "CHADTree"
 }
-vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_colors = {
+  bg_float = clrFloat,
+}
 
-require("tokyonight").colorscheme()
+vim.cmd [[colorscheme tokyonight]]
 
-vim.cmd([[hi! link TabLineSel Special]])
-vim.cmd([[hi! TabLine guifg=#545c7e]])
-vim.cmd([[hi! TabLineMod guifg=#f7768e]])
-vim.cmd([[hi! TabLineModBuf guifg=#9ece6a]])
-vim.cmd([[hi! TabLineModNone guifg=#bb9af7]]) -- 1a1b26
+vim.cmd [[hi! link TabLineSel Special]]
+vim.cmd [[hi! TabLine guifg=#545c7e]]
+vim.cmd [[hi! TabLineMod guifg=#f7768e]]
+vim.cmd [[hi! TabLineModBuf guifg=#9ece6a]]
+vim.cmd [[hi! TabLineModNone guifg=#bb9af7]]
+
+vim.cmd(string.format("hi TelescopeNormal guibg=%s", clrFloat))
+vim.cmd(string.format("hi TelescopeBorder guibg=%s", clrFloat))
 
 -- vim.cmd('colorscheme tokyonight')
