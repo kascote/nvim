c340719c0522ca225b87b056d085ca415c648b28
Nelson Fernandez <nelson@netflux.com.ar>
2022-05-08 21:19:52 -0300

    update dart comments and remove single comments by default

--- lua/nfx/plugins/comments.lua
+++ lua/nfx/plugins/comments.lua
@@ -1,5 +1,12 @@
-require("kommentary.config").configure_language("default", {
-  prefer_single_line_comments = true,
+local c = require "kommentary.config"
+
+c.configure_language("default", {
+  -- prefer_single_line_comments = true,
   use_consistent_indentation = true,
   ignore_whitespace = true,
+})
+
+c.configure_language("dart", {
+  single_line_comment_string = "//",
+  multi_line_comment_strings = { "/*", "*/" },
 })
