0a2849edb87f7d9c9a8745718200753c05d5d540
nelson fernandez <nelson@netflux.com.ar>
2022-07-30 18:52:58 -0300

    updated lualine

--- lua/nv/plugins/lualine.lua
+++ lua/nv/plugins/lualine.lua
@@ -1,12 +1,12 @@
 local fn = vim.fn
 local api = vim.api
 
-local lineDisplay = {
+--[[ local lineDisplay = {
   bufNameLen = 0,
   tabPageLen = 0,
-}
+} ]]
 
-local function lsp_progress()
+--[[ local function lsp_progress()
   local messages = vim.lsp.util.get_progress_messages()
   if #messages == 0 then
     return ''
@@ -19,23 +19,23 @@ local function lsp_progress()
   local ms = vim.loop.hrtime() / 1000000
   local frame = math.floor(ms / 120) % #spinners
   return table.concat(status, " | ") .. " " .. spinners[frame + 1]
-end
+end ]]
 
 local function fullname()
   return vim.fn.expand "%:f"
 end
 
-local function middle()
+--[[ local function middle()
   return "%="
-end
+end ]]
 
 -- fix a bug when is null
 local function nada()
   return " "
 end
 
-local function countModified(bufName)
-  -- [[ read in reverse order ]]
+--[[ local function countModified(bufName)
+  -- read in reverse order
   return fn.count( -- cound the modified buffers
     fn.map( -- map to an array with the changed values
       fn.filter( -- remove the current buffer from the count
@@ -51,9 +51,9 @@ local function countModified(bufName)
     ),
     1
   ) > 0
-end
+end ]]
 
-local function formatBufferName(bname)
+--[[ local function formatBufferName(bname)
   local basePath = fn.fnamemodify(bname, ":p:~:.:h")
   local baseName = fn.fnamemodify(bname, ":p:t")
   local label
@@ -69,9 +69,9 @@ local function formatBufferName(bname)
   -- length of the buffer title
   lineDisplay.bufNameLen = #labelRaw
   return label
-end
+end ]]
 
-local function filename()
+--[[ local function filename()
   local bufName = api.nvim_buf_get_name(0)
   local modifiedCurrent = fn.getbufinfo("%")[1].changed
   local modifiedBuffers = countModified(bufName)
@@ -88,24 +88,24 @@ local function filename()
   lineDisplay.bufNameLen = lineDisplay.bufNameLen + 1
 
   return label .. formatBufferName(bufName) .. currentMod
-end
+end ]]
 
 local function mydiff()
   local ok, res = pcall(vim.api.nvim_buf_get_var, 0, "gitsigns_status_dict")
   local diff = {}
   if ok then
     if res.added ~= 0 and res.added ~= nil then
-      table.insert(diff, "%#DiffAdd#+" .. res.added .. " %*")
+      table.insert(diff, "%#StatusLineDiffAdd# +" .. res.added .. " %*")
     end
     if res.removed ~= 0 and res.removed ~= nil then
-      table.insert(diff, "%#DiffDelete# -" .. res.removed .. " %*")
+      table.insert(diff, "%#StatusLineDiffDel# -" .. res.removed .. " %*")
     end
     if res.changed ~= 0 and res.changed ~= nil then
-      table.insert(diff, "%#DiffChange# ~" .. res.changed .. " %*")
+      table.insert(diff, "%#StatusLineDiffChg# ~" .. res.changed .. " %*")
     end
     return table.concat(diff, "")
   else
-    return ''
+    return ""
   end
 end
 
@@ -122,7 +122,13 @@ local config = {
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { { "diagnostics", sources = { "nvim_diagnostic" } } },
+    lualine_b = {
+      {
+        "diagnostics",
+        sources = { "nvim_diagnostic" },
+        color = { bg = "#313244" },
+      },
+    },
     lualine_c = { mydiff },
     lualine_x = { nada },
     lualine_y = {},
@@ -138,10 +144,10 @@ local config = {
   },
   tabline = {
     lualine_a = {},
-    lualine_b = { "branch" },
-    lualine_c = { { middle, separator = "" }, { filename } },
-    lualine_x = { "filetype" },
-    lualine_y = { "encoding" },
+    lualine_b = { { "branch", color = { bg = "#1e1e2e" } } },
+    lualine_c = {}, -- { { middle, separator = "" }, { filename } },
+    lualine_x = { { "filetype", color = { fg = "#f2cdcd", gui = "italic" } } },
+    lualine_y = { { "encoding", color = { bg = "#1e1e2e" } } },
     lualine_z = {},
   },
   extensions = { "chadtree", "quickfix" },
