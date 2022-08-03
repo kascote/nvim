3d3805ad84d58ffa47f9a0c4b712992a4cb47d96
nelson fernandez <nelson@netflux.com.ar>
2022-08-02 22:06:23 -0300

    updated lualine... again

--- lua/nv/plugins/lualine.lua
+++ lua/nv/plugins/lualine.lua
@@ -1,94 +1,101 @@
 local fn = vim.fn
 local api = vim.api
 
---[[ local lineDisplay = {
+local lineDisplay = {
   bufNameLen = 0,
   tabPageLen = 0,
-} ]]
+}
 
---[[ local function lsp_progress()
-  local messages = vim.lsp.util.get_progress_messages()
-  if #messages == 0 then
-    return ''
-  end
-  local status = {}
-  for _, msg in pairs(messages) do
-    table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
-  end
-  local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-  local ms = vim.loop.hrtime() / 1000000
-  local frame = math.floor(ms / 120) % #spinners
-  return table.concat(status, " | ") .. " " .. spinners[frame + 1]
-end ]]
+local filetypeExclude = {
+  "",
+  "help",
+  "startify",
+  "dashboard",
+  "packer",
+  "neogitstatus",
+  "NvimTree",
+  "Trouble",
+  "alpha",
+  "lir",
+  "Outline",
+  "spectre_panel",
+  "toggleterm",
+  "TelescopePrompt",
+  "floaterm",
+}
 
 local function fullname()
   return vim.fn.expand "%:f"
 end
 
---[[ local function middle()
+local function middle()
   return "%="
-end ]]
+end
 
 -- fix a bug when is null
 local function nada()
   return " "
 end
 
---[[ local function countModified(bufName)
+local function countModified(bufName)
   -- read in reverse order
   return fn.count( -- cound the modified buffers
     fn.map( -- map to an array with the changed values
-      fn.filter( -- remove the current buffer from the count
+      -- fn.filter( -- remove the current buffer from the count
         fn.filter( -- get only the listed buffers
           fn.copy( -- get a copy of the buffer
             fn.getbufinfo()
           ),
           "v:val.listed"
         ),
-        "v:val.name !~ '" .. bufName .. "'"
-      ),
+        -- "v:val.name !~ '" .. bufName .. "'"
+      -- ),
       "v:val.changed"
     ),
     1
   ) > 0
-end ]]
+end
 
---[[ local function formatBufferName(bname)
+local function formatBufferName(bname)
+
+  if vim.tbl_contains(filetypeExclude, vim.bo.filetype) then
+    return " %#StatusLineFilename#" .. vim.bo.filetype
+  end
+
   local basePath = fn.fnamemodify(bname, ":p:~:.:h")
   local baseName = fn.fnamemodify(bname, ":p:t")
   local label
   local labelRaw
   if bname == "" then
-    label = " %#TabLine#(empty) "
+    label = " %#StatusLineFilename#(empty) "
     labelRaw = " (empty) "
   else
-    label = " %#TabLine#" .. basePath .. "/" .. "%#TabLineSel#" .. baseName .. " %*"
+    label = " %#StatusLineFilename#" .. basePath .. "/" .. "%#StatusLineFilenameHi#" .. baseName .. " %*"
     labelRaw = " " .. basePath .. "/" .. baseName .. " "
   end
 
   -- length of the buffer title
   lineDisplay.bufNameLen = #labelRaw
   return label
-end ]]
+end
 
---[[ local function filename()
+local function filename()
   local bufName = api.nvim_buf_get_name(0)
-  local modifiedCurrent = fn.getbufinfo("%")[1].changed
   local modifiedBuffers = countModified(bufName)
-  local label = "%#TabLineModNone#" .. " "
+  local label = "%#DiffDelete#" .. " "
 
   if modifiedBuffers then
-    label = "%#TabLineMod#" .. "●"
+    label = "%#DiffDelete#" .. "●"
   end
 
-  local currentMod = " "
+  --[[ local currentMod = " "
   if modifiedCurrent == 1 then
     currentMod = "%#TabLineModBuf#" .. "●"
-  end
+  end ]]
   lineDisplay.bufNameLen = lineDisplay.bufNameLen + 1
 
-  return label .. formatBufferName(bufName) .. currentMod
-end ]]
+  return label .. formatBufferName(bufName) -- .. currentMod
+end
 
 local function mydiff()
   local ok, res = pcall(vim.api.nvim_buf_get_var, 0, "gitsigns_status_dict")
@@ -145,7 +152,7 @@ local config = {
   tabline = {
     lualine_a = {},
     lualine_b = { { "branch", color = { bg = "#1e1e2e" } } },
-    lualine_c = {}, -- { { middle, separator = "" }, { filename } },
+    lualine_c = { { middle, separator = "" }, { filename } },
     lualine_x = { { "filetype", color = { fg = "#f2cdcd", gui = "italic" } } },
     lualine_y = { { "encoding", color = { bg = "#1e1e2e" } } },
     lualine_z = {},
