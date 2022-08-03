a11b91415c9c6aefe86149977b820b6715eff639
nelson fernandez <nelson@netflux.com.ar>
2022-08-02 22:06:38 -0300

    some snipets update

--- lua/nv/plugins/snipets.lua
+++ lua/nv/plugins/snipets.lua
@@ -1,5 +1,11 @@
 local ls = require "luasnip"
 local ps = ls.parser.parse_snippet
+local s = ls.snippet
+-- local c = ls.choice_node
+-- local t = ls.text_node
+local fmt = require("luasnip.extras.fmt").fmt
+local i = ls.insert_node
+-- local sn = ls.snippet_node
 
 ls.add_snippets("all", {
   ps("exp", "-- testing"),
@@ -14,3 +20,41 @@ ls.add_snippets("dart", {
   ps("try", "try {\n  $1\n} catch (e) {\n\n} finally {\n}"),
   ps("sleep", "sleep(Duration(seconds:$1));"),
 })
+
+local js_snippets = {
+  ps(
+    "jdesc",
+    [[
+describe("$1", ()=> {
+  $2
+});
+  ]]
+  ),
+
+  ps(
+    "jit",
+    [[
+it("$1", () => {
+  $2
+})
+  ]]
+  ),
+
+  s(
+    "import",
+    fmt([[import {1} from "{2}";]], {
+      i(1, ""),
+      i(2, "path"),
+    })
+  ),
+
+  ps("iface", [[
+interface $1 {
+  $2
+}]]),
+}
+
+ls.add_snippets("javascript", js_snippets)
+ls.add_snippets("typescript", js_snippets)
+ls.add_snippets("javascriptreact", js_snippets)
+ls.add_snippets("typescriptreact", js_snippets)
